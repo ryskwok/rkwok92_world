10f345ee5b51f765000a0aa4a1432ee937b00686
Darren <drriley@andrew.cmu.edu>
2012-04-25 10:37:56 -0400

    article model

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+  belongs_to :category
+  validates_presence_of :title, :content
+  
+  scope :alphabetical, order('title') 
+  scope :active, where('active = ?', true)
+end
